e3680cff059f6e21a1eececf487633e0ece3257e
Sakri Rosenstrom <sakri.rosenstrom@gmail.com>
2013-10-01 15:42:33 +0200

    harmonize animations

--- demos/Demos.js
+++ demos/Demos.js
@@ -270,7 +270,8 @@
 		this.canvas.addEventListener("click", function(event){_this.canvasClickHandler(event)}, false);//"mousedown"
 		this.barChartOpen = true;
 		this.animator = new UnitAnimator(1500,20,function() {_this.updateBarChart()});
-		this.animator.start(UnitAnimator.getRandomEasingFunction());
+		//this.animator.start(UnitAnimator.getRandomEasingFunction());
+		this.animator.start(UnitAnimator.easeOutSine);
 	}
 
 	BarChartDemo.prototype.updateBarChart = function(){
@@ -288,7 +289,7 @@
 		}
 		var _this = this;
 		this.animator.reset(1500,20,this.barChartOpen ? function(event){_this.updateBarChartReverse();} : function(event){_this.updateBarChart();} );
-		this.animator.start(UnitAnimator.getRandomEasingFunction());
+		this.animator.start(this.barChartOpen ? UnitAnimator.easeInSine : UnitAnimator.easeOutSine);
 		this.barChartOpen =! this.barChartOpen;
 	}
 	
@@ -597,14 +598,14 @@
 		this.intro = true;
 		this.blockSetAnimator = new BlockSetAnimator(this.x, this.y+this.height/2 - this.imageStore.images[0].height/2, this.width, this.height);
 		this.blockSetAnimator.setImages(this.imageStore.images);
-		this.runRandomAnimation();
+		BlockSetAnimatorDemo.runRandomAnimation(this);
 	}
 	
 	BlockSetAnimatorDemo.prototype.canvasClickHandler = function(event){
 		if(this.blockSetAnimator.isAnimating()){
 			return;
 		}
-		this.runRandomAnimation();
+		BlockSetAnimatorDemo.runRandomAnimation(this);
 	}
 	
 	BlockSetAnimatorDemo.getIntroEasingFunction = function(){
@@ -625,24 +626,35 @@
 		return easing[Math.floor( Math.random()*easing.length )];	
 	}
 	
-	BlockSetAnimatorDemo.prototype.runRandomAnimation = function(event){
+	//used for both image blocks and text effect demo  TODO : clean this up, too many conditionals
+	BlockSetAnimatorDemo.runRandomAnimation = function(demo){
 		//console.log("BlockSetAnimatorDemo.runRandomAnimation()");
-		var rotation = -Math.PI*3 + Math.random() * Math.PI*6;
+		var easingFunction = demo.intro ? BlockSetAnimatorDemo.getIntroEasingFunction() : BlockSetAnimatorDemo.getEndtroEasingFunction();
+		var rotation;
+		var minTranform;
+		var maxTranform;
+		if( easingFunction==UnitAnimator.easeLinear || easingFunction==UnitAnimator.easeOutSine || UnitAnimator.easeInSine){
+			rotation  = -Math.PI*3 + Math.random() * Math.PI*6;
+			minTranform = -200;
+			maxTranform = 400;
+		}else{
+			rotation  = -Math.PI*1.5 + Math.random() * Math.PI*3;
+			minTranform = -100;
+			maxTranform = 200;
+		}
 		var scale = Math.random() * 3;
 		var hAlign = Math.floor(Math.random()*3);
 		var vAlign = Math.floor(Math.random()*3);
-		var transformA = new AnimationBlockTransform(-200 + Math.random()*400, -200 + Math.random()*400, scale, rotation, 0, hAlign, vAlign );
+		var transformA = new AnimationBlockTransform(minTranform + Math.random()*maxTranform, minTranform + Math.random()*maxTranform, scale, rotation, 0, hAlign, vAlign );
 		var transformB = new AnimationBlockTransform(0, 0, 1, 0, 1, hAlign, vAlign);
-		if(this.intro){
-			this.blockSetAnimator.setAnimation(transformA, transformB);
-			this.blockSetAnimator.setEasingFunction(BlockSetAnimatorDemo.getIntroEasingFunction());
+		if(demo.intro){
+			demo.blockSetAnimator.setAnimation(transformA, transformB);
 		}else{
-			this.blockSetAnimator.setAnimation(transformB, transformA);
-			this.blockSetAnimator.setEasingFunction(BlockSetAnimatorDemo.getEndtroEasingFunction());
+			demo.blockSetAnimator.setAnimation(transformB, transformA);
 		}
-		var _this = this;
-		this.blockSetAnimator.start (2000, 250, function(){_this.animationComplete()} , function(){_this.animationUpdate()});
-		this.intro = !this.intro;
+		demo.blockSetAnimator.setEasingFunction(easingFunction);
+		demo.blockSetAnimator.start (2000, 250, function(){demo.animationComplete()} , function(){demo.animationUpdate()});
+		demo.intro = !demo.intro;
 	}
 	
 	BlockSetAnimatorDemo.prototype.animationUpdate = function(){
@@ -684,35 +696,14 @@
 		var images = textChopper.createImagesFromString(this.document,"CANVAS",80, "#AAAAFF", "#000044");
 		this.blockSetAnimator = new BlockSetAnimator( this.x , this.y + this.height/2 - images[0].height/2, this.width, this.height);
 		this.blockSetAnimator.setImages(images);
-		this.runRandomAnimation();
+		BlockSetAnimatorDemo.runRandomAnimation(this);
 	}
 	
 	TextEffectDemo.prototype.canvasClickHandler = function(event){
 		if(this.blockSetAnimator.isAnimating()){
 			return;
 		}
-		this.runRandomAnimation();
-	}
-	
-	TextEffectDemo.prototype.runRandomAnimation = function(event){
-		//console.log("TextEffectDemo.runRandomAnimation()");
-		var rotation = -Math.PI*4 + Math.random() * Math.PI*8;
-		var scale = Math.random() * 3;
-		var hAlign = Math.floor(Math.random()*3);
-		var vAlign = Math.floor(Math.random()*3);
-		var transformA = new AnimationBlockTransform(-200 + Math.random()*400, -200 + Math.random()*400, scale, rotation, 0, hAlign, vAlign );
-		var transformB = new AnimationBlockTransform(0, 0, 1, 0, 1, hAlign, vAlign);
-		if(this.intro){
-			this.blockSetAnimator.setAnimation(transformA, transformB);
-			this.blockSetAnimator.setEasingFunction(BlockSetAnimatorDemo.getIntroEasingFunction());
-		}else{
-			this.blockSetAnimator.setAnimation(transformB, transformA);
-			this.blockSetAnimator.setEasingFunction(BlockSetAnimatorDemo.getEndtroEasingFunction());
-		}
-		this.blockSetAnimator.setEasingFunction(UnitAnimator.getRandomEasingFunction());
-		var _this = this;
-		this.blockSetAnimator.start (2000, 250, function(){_this.animationComplete()} , function(){_this.animationUpdate()});
-		this.intro = !this.intro;
+		BlockSetAnimatorDemo.runRandomAnimation(this);
 	}
 	
 	TextEffectDemo.prototype.animationUpdate = function(){
